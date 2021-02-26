9240282769549733b592b994ee201804f89a50a5
Howard Huang <howardhuang96@gmail.com>
2021-02-25 20:37:00 -0800

    add storybook and storybook example code

---
+++ frontend/.storybook/preview.js
@@ -0,0 +1,8 @@
+// .storybook/preview.js
+
+import "../src/index.css"; //👈 The app's CSS file goes here
+
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+};
