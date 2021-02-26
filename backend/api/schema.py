3d402176142b672a85144f522c31d8a2e6ac51fe
Howard Huang <howardhuang96@gmail.com>
2021-02-26 07:23:20 -0800

    set up relay in django

--- backend/api/schema.py
+++ backend/api/schema.py
@@ -1,5 +1,7 @@
 import graphene
+from graphene import relay, ObjectType
 from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 
 from api.models import *
 
@@ -9,11 +11,48 @@ from graphql_jwt.decorators import (login_required, user_passes_test)
 from django.conf import settings
 
 
+class Ship(graphene.ObjectType):
+    '''A ship in the Star Wars saga'''
+    class Meta:
+        interfaces = (relay.Node, )
+
+    name = graphene.String(description='The name of the ship.')
+
+    @classmethod
+    def get_node(cls, info, id):
+        return get_ship(id)
+
+
+class CategoryNode(DjangoObjectType):
+    class Meta:
+        model = Category
+        filter_fields = ['name', 'ingredients']
+        interfaces = (relay.Node, )
+
+
+class IngredientNode(DjangoObjectType):
+    class Meta:
+        model = Ingredient
+        # Allow for some more advanced filtering here
+        filter_fields = {
+            'name': ['exact', 'icontains', 'istartswith'],
+            'notes': ['exact', 'icontains'],
+            'category': ['exact'],
+            'category__name': ['exact'],
+        }
+        interfaces = (relay.Node, )
+
+
 class Query(graphene.ObjectType):
     '''
     Documentation for base query
     '''
     hello = graphene.String()
+    category = relay.Node.Field(CategoryNode)
+    all_categories = DjangoFilterConnectionField(CategoryNode)
+
+    ingredient = relay.Node.Field(IngredientNode)
+    all_ingredients = DjangoFilterConnectionField(IngredientNode)
 
 
 class Mutation(graphene.ObjectType):
--- backend/server/settings.py
+++ backend/server/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'graphene_django',
 
     # own apps
+    'api',
 ]
 
 MIDDLEWARE = [
